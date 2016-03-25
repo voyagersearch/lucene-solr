9d9a7d6dafd18dc01a814280b8cd49fba7b05dfd
Ryan McKinley <ryantxu@gmail.com>
2016-03-25 13:23:58 -0700

    Update QueryTransformerFactory.java

--- solr/core/src/java/org/apache/solr/response/transform/QueryTransformerFactory.java
+++ solr/core/src/java/org/apache/solr/response/transform/QueryTransformerFactory.java
@@ -16,6 +16,100 @@
  */
 package org.apache.solr.response.transform;
 
-public class QueryTransformerFactory {
+import java.io.IOException;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrException.ErrorCode;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.BasicResultContext;
+import org.apache.solr.response.transform.DocTransformer;
+import org.apache.solr.response.transform.TransformerFactory;
+import org.apache.solr.search.DocList;
+import org.apache.solr.search.QParser;
+import org.apache.solr.search.QParserPlugin;
+import org.apache.solr.search.SolrReturnFields;
+import org.apache.solr.search.SyntaxError;
 
+import com.natelenergy.drop3.solr.SolrClientUtil;
+
+
+public class QueryTransformerFactory extends TransformerFactory
+{ 
+  @Override
+  public DocTransformer create(String field, SolrParams params, SolrQueryRequest req) {
+    
+    Query filter = null;
+    String fq = params.get("fq");
+    if(fq!=null) {
+      QParserPlugin qpp = req.getCore().getQueryPlugin(QParserPlugin.DEFAULT_QTYPE);
+      
+      QParser pp = qpp.createParser(fq, null, params, req);
+      try {
+        filter = pp.getQuery();
+      } 
+      catch (SyntaxError e) {
+        throw new RuntimeException(e);
+      }
+    }
+    
+    String joinField = params.get("j");
+    if(joinField ==null) {
+      throw new SolrException(ErrorCode.BAD_REQUEST, "Missing param 'j' for join field");
+    }
+    
+    String subfl = params.get("sub.fl");
+    if(subfl==null) {
+      subfl = req.getParams().get("sub.fl");
+    }
+    SolrReturnFields rf = new SolrReturnFields(subfl, req);
+    return new SubQueryAugmenter(field, joinField, filter, rf);
+  }
+}
+
+class SubQueryAugmenter extends DocTransformer
+{
+  final Query filter;
+  final String name;
+  final String joinField;
+  final SolrReturnFields rf;
+  
+  public SubQueryAugmenter(String name, String joinField, Query filter, SolrReturnFields rf) {
+    this.name = name;
+    this.joinField = joinField;
+    this.rf = rf;
+    this.filter = filter;
+  }
+
+  @Override
+  public String[] getExtraRequestFields() {
+    return new String[] {"id"};
+  }
+  
+  @Override
+  public String getName() {
+    return "SubQueryAugmenter";
+  }
+
+  @Override
+  public void transform(SolrDocument doc, int docid, float score) throws IOException {
+    String id = SolrClientUtil.getString("id", doc);
+    
+    if(context.getQuery()==null || id==null) {
+      return; // this happens for the sub query
+    }
+
+    // TODO, use QParser so we pick the right kind of query!
+    TermQuery q = new TermQuery(new Term(joinField, id));
+    DocList docs = context.getSearcher().getDocList(q, filter, null, 0, 10);
+    if(docs!=null) {
+
+      BasicResultContext rc = new BasicResultContext(docs, rf, context.getSearcher(), q, null);
+
+      doc.setField(name, rc);
+    }
+  } 
 }
