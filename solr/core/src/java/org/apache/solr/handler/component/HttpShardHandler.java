2cbda12c67244102054755fac5b26f63be6efc5a
jdeolive <jdeolive@gmail.com>
2016-11-29 21:03:37 -0700

    Adding hooks to customize solr client creation in http shard handler.
    Used to apply custom authorization logic when querying shards.

--- solr/core/src/java/org/apache/solr/handler/component/HttpShardHandler.java
+++ solr/core/src/java/org/apache/solr/handler/component/HttpShardHandler.java
@@ -159,7 +159,7 @@ public class HttpShardHandler extends ShardHandler {
         if (urls.size() <= 1) {
           String url = urls.get(0);
           srsp.setShardAddress(url);
-          try (SolrClient client = new Builder(url).withHttpClient(httpClient).build()) {
+          try (SolrClient client = makeSolrClient(url)) {
             ssr.nl = client.request(req);
           }
         } else {
@@ -197,7 +197,14 @@ public class HttpShardHandler extends ShardHandler {
       MDC.remove("ShardRequest.urlList");
     }
   }
-  
+
+  /**
+   * Subclass hook for creating solr client.
+   */
+  protected SolrClient makeSolrClient(String url) {
+    return new Builder(url).withHttpClient(httpClient).build();
+  }
+
   /**
    * Subclasses could modify the request based on the shard
    */
